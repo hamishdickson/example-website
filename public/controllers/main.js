d02d87e03e57cf7b2c2494383cd8ba5605b9ec7d
mishyTheBear <hamish.dickson@gmail.com>
2014-08-16 20:57:07 +0100

    Add email stuff to the contacts

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -17,17 +17,41 @@
 
     }]);
 
-    app.controller('MessageController', function() {
+    app.controller('MessageController', ['$scope', '$alert', '$http', function($scope, $alert, $http) {
         this.message = {};
 
         this.addMessage = function (message) {
+            $http.post('/api/message', message)
+                .success( function() {
+                    $alert({
+                        title: 'Cheers!',
+                        content: 'Thanks for messaging me - I will get back to you as soon as I can!',
+                        placement: 'top-right',
+                        type: 'success',
+                        duration: 3
+                    });
+                })
+                .error( function() {
+                    $alert({
+                        title: 'Eh no!',
+                        content: 'Thanks for messaging me - but there was a problem, please contact me via linkedIn!',
+                        placement: 'top-right',
+                        type: 'danger',
+                        duration: 10
+                    });
+                });
 
+            // clear out the preview
             this.message = {};
+
+            // remove the styling
+            $scope.contactForm.$setPristine();
         };
-    });
+    }]);
 
     var messages = [{
         "body": "",
+        "name": "",
         "email": ""
     }];
 })();
--- server.js
+++ server.js
@@ -1,18 +1,46 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var nodemailer = require('nodemailer');
 var app = express();
+
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), function() {
+app.post('/api/message', function (req, res) {
+    var smtpTransport = nodemailer.createTransport({
+        service: 'Mailgun',
+        auth: {
+            user: 'postmaster@sandboxda54eeedc2fd46a8bff340643c3969ec.mailgun.org',
+            pass: '8fa219eeb2096e0516d242a382ccaafa'
+        }
+    });
+
+    var mailOptions = {
+        from: 'messages@snowbear.com',
+        to: 'hamish.dickson@gmail.com',
+        subject: 'You have a new message from ' + req.body.name + '!',
+        text: 'Message: ' + req.body.body + '\n\nName: ' + req.body.name + '\n\nEmail: ' + req.body.email
+    };
+
+    smtpTransport.sendMail(mailOptions, function (error, response) {
+        if (error) {
+            console.log('Error sending email: ' + error);
+            smtpTransport.close();
+            res.send(500);
+        } else {
+            smtpTransport.close();
+            console.log('Email sent to Hamish: ' + req.body.body);
+            res.send(200);
+        }
+    });
+});
+
+app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
